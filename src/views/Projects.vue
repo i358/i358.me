489117f9e64e63efe7a2d6033aa5683efcf5f5e3
i358 <retro98team@gmail.com>
2021-08-14 17:51:36 +0300

    update: Projects.vue [FX3]

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,15 +1,23 @@
 <template>
-
+<div>
+    {{cache.filter(n=>n.t==="test")}}
+</div>
 </template>
 <script>
+import { get } from 'axios'
 export default { 
     data(){
         return {
-
+          ENDPOINTS:{
+              GITHUB_USER:"https://api.github.com/users/i358/repos",
+              GITHUB_USER_REPOS:"https://api.github.com/users/i358/repos"
+          },
+          cache:[{t:"repos"}]
         }
-    },
+    }, 
     computed(){
-        
+        get(this.ENDPOINTS.GITHUB_USER_REPOS)
+        .then(({ data })=>this.cache.push({t:"test", val:data}))
     }
 }
 </script>
