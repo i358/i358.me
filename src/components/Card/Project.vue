bb9034ccbe0a310dde9bc6955e735735149e5183
i358 <retro98team@gmail.com>
2021-08-20 18:01:13 +0300

    update: projec card

--- src/components/Card/Project.vue
+++ src/components/Card/Project.vue
@@ -2,7 +2,7 @@
     <div class="bg-[#23263a] w-[340px] h-[200px] rounded-[10px] grid-rows-[2fr,8fr] gap-[15px] p-[17px] grid project">
       <div class="grid grid-cols-[auto,10fr,3fr] gap-[10px] items-center">
           <i class="fa fa-github text-[35px]"></i>
-          <h1 class="text-[19px] font-medium cursor-pointer hover:underline">{{project.full_name}}</h1>
+          <h1 class="text-[19px] font-medium cursor-pointer hover:underline" @click="linkTo(fuckRules(project.full_name))">{{project.full_name}}</h1>
           <p class="font-bold text-[orange] flex items-center justify-center gap-[7px]"><i class="fa fa-star font-bold"></i> <span id="stars">{{project.stargazers_count}}</span> </p>
       </div>
       <div>
@@ -21,6 +21,14 @@ export default {
     data(){
         return {
 
+        }
+    },
+    methods:{
+        linkTo(x){
+            window.location = x
+        },
+        fuckRules(x){
+            return `https://github.com/${x}`;
         }
     },
     props:["project"]
